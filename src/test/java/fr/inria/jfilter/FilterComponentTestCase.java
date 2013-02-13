86185eaa4de769ffbc69f250a7ceeabbdaac697e
Francois Fouquet <fouquet.f@gmail.com>
2013-02-13 10:06:27 +0100

    add two tests (failed :-))

---
+++ src/test/java/fr/inria/jfilter/FilterComponentTestCase.java
@@ -0,0 +1,11 @@
+package fr.inria.jfilter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: duke
+ * Date: 13/02/13
+ * Time: 09:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class FilterComponentTestCase {
+}
---
+++ src/test/java/fr/inria/jfilter/functional/CollectionFunctionalTEst.java
@@ -0,0 +1,11 @@
+package fr.inria.jfilter.functional;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: duke
+ * Date: 13/02/13
+ * Time: 09:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class CollectionFunctionalTEst {
+}
